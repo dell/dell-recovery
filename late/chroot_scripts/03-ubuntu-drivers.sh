06b9eeb2e77f9b6595be085e479ddd3418134b04
chinyakao <riley.kao@iss.nthu.edu.tw>
2021-07-15 15:16:46 +0800

    fix for loop in build package

--- late/chroot_scripts/03-ubuntu-drivers.sh
+++ late/chroot_scripts/03-ubuntu-drivers.sh
@@ -6,10 +6,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 IFHALT "Run ubuntu-drivers autoinstall"
 echo 'APT::Get::AllowUnauthenticated "true";' > /etc/apt/apt.conf.d/99disable_authentication
-# for blacklist in $(find /cdrom/scripts/chroot-scripts/blacklist /isodevice/scripts/chroot-scripts/blacklist -type f 2>/dev/null); do
-while IFS= read -r -d '' blacklist; do
+# while IFS= read -r -d '' blacklist; do
+#     UBUNTU_DRIVERS_BLACKLIST="$UBUNTU_DRIVERS_BLACKLIST $(cat "$blacklist")"
+# done < <(find /cdrom/scripts/chroot-scripts/blacklist /isodevice/scripts/chroot-scripts/blacklist -type f -print0 2>/dev/null)
+for blacklist in $(find /cdrom/scripts/chroot-scripts/blacklist /isodevice/scripts/chroot-scripts/blacklist -type f 2>/dev/null); do
     UBUNTU_DRIVERS_BLACKLIST="$UBUNTU_DRIVERS_BLACKLIST $(cat "$blacklist")"
-done < <(find /cdrom/scripts/chroot-scripts/blacklist /isodevice/scripts/chroot-scripts/blacklist -type f 2>/dev/null)
+done
 if [ -n "$UBUNTU_DRIVERS_BLACKLIST" ]; then
     echo "UBUNTU_DRIVERS_BLACKLIST: $UBUNTU_DRIVERS_BLACKLIST"
 fi
